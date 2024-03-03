659d5d2dacd1290701fbb6b8b3f5c36ee19ff87f
ugyentring <12220096.gcit@rub.edu.bt>
2024-03-03 13:01:19 +0600

    all setup configured

--- src/app.js
+++ src/app.js
@@ -23,7 +23,13 @@ app.use(
   })
 );
 
-app.use(express.static("publuc"));
+app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ dotenv.config({
 });
 
 import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
 
 connectDB()
   .then(() => {
